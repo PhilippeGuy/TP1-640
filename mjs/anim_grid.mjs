15c7346aa01d5720ae0b126f4bcd8d8395efc9ab
Philippe Guy <philippeguy@hotmail.com>
2020-02-18 20:55:38 -0500

    ajout de la function activerHover dans anim_grid.mjs

--- mjs/anim_grid.mjs
+++ mjs/anim_grid.mjs
@@ -3,14 +3,27 @@ export class anim_grid{
         this.elmGrid = elmGrid;
     }
 
+    
     animerGrid(){
-        console.log(this.elmGrid);
         const grid = this.elmGrid;
+
+        //initialise le délais entre les carrées
         let delay =0;
+
+        //ajoute la classe pour l'animation et le délais pour chaque élément du grid
         for(let i=0; i<grid.length; i++){
             grid[i].classList.add('animerGrid');
             grid[i].style.animationDelay = delay+"s";
+            grid[i].addEventListener("animationend", this.activerHover);
             delay += 0.25;
         }
     }
+
+    activerHover(){
+        this.classList.remove('animerGrid');
+        this.classList.add('gridHover');
+        this.removeEventListener("animationend", this.activerHover);
+    }
+
+    
 }
